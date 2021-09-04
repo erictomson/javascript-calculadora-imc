3202850d12b1290bae2b9bfc3fdd7dda6ffeec94
erictomson <erictomson@gmail.com>
2021-09-04 14:21:43 -0300

    Exibindo os dados diretamente na página HTML

--- js/controller.js
+++ js/controller.js
@@ -8,6 +8,15 @@ var peso = 76.00;
 var altura = 1.78;
 var imc = peso / (altura**2); // imc
 
-console.log(nome);
-console.log(`Peso: ${peso} - Altura: ${altura}`); //string template
-console.log(`Cálculo do IMC: ${imc.toFixed(2)}`);
+var elNome = document.getElementById("nome");
+elNome.innerHTML = nome;
+var elPeso = document.getElementById("peso");
+elPeso.innerHTML = peso;
+var elAltura = document.getElementById("altura");
+elAltura.innerHTML = altura;
+var elImc = document.getElementById("imc");
+elImc.innerHTML = imc.toFixed(2);
+
+// console.log(nome);
+// console.log(`Peso: ${peso} - Altura: ${altura}`); //string template
+// console.log(`Cálculo do IMC: ${imc.toFixed(2)}`);
