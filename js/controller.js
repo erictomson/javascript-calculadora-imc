917877f497d7a8dc02cd0916763760139a5dab4c
erictomson <erictomson@gmail.com>
2021-09-04 14:44:24 -0300

    Determinando a situação a partir do IMC

--- js/controller.js
+++ js/controller.js
@@ -7,6 +7,7 @@ var nome = "Renato Oliveira";
 var peso = 76.00;
 var altura = 1.78;
 var imc = peso / (altura**2); // imc
+var situacao = null; //vazio
 
 var elNome = document.getElementById("nome");
 elNome.innerHTML = nome;
@@ -17,6 +18,18 @@ elAltura.innerHTML = altura;
 var elImc = document.getElementById("imc");
 elImc.innerHTML = imc.toFixed(2);
 
+if (imc < 18.5) {
+    situacao = "Magreza";
+} else if (imc >= 18.5 && imc <= 24.9) {
+    situacao = "Normal";
+} else if (imc >= 24.9 && imc <= 30) {
+    situacao = "Sobrepeso";
+} else {
+    situacao = "Obesidade";
+}
+
+var elSituacao = document.getElementById("situacao");
+elSituacao.innerHTML = situacao;
 // console.log(nome);
 // console.log(`Peso: ${peso} - Altura: ${altura}`); //string template
 // console.log(`Cálculo do IMC: ${imc.toFixed(2)}`);
